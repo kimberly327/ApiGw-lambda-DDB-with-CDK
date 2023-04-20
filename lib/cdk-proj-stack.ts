9c1786e31198e9bc5aaefe701ee80a2608f75aab
kimberly327 <caziero.kimberly@gmail.com>
2023-04-20 14:39:51 +0200

    api integrated directly with DynamoDB

--- lib/cdk-proj-stack.ts
+++ lib/cdk-proj-stack.ts
@@ -1,16 +1,85 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps }from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-export class CdkProjStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkProjStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dynamodb_table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const integrationRole = new iam.Role(this, 'IntegrationRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')
+    });
+
+    dynamodb_table.grantReadWriteData(integrationRole);
+
+    const api = new apigateway.RestApi(this, 'api', {});
+
+    const sendMessageIntegration = new apigateway.AwsIntegration({
+      service: 'dynamodb',
+      integrationHttpMethod: 'POST',
+      action: 'PutItem',
+      options: {
+        credentialsRole: integrationRole,
+        requestParameters: {
+          'integration.request.querystring.who': 'method.request.querystring.who'
+        },
+        requestTemplates: {
+          'application/json': JSON.stringify({
+            'TableName': dynamodb_table.tableName,
+            'Item': {
+              'PK': { 'S': "$context.requestTimeEpoch" },
+              'SK': { 'S': "Detail" },
+              'userId': {"S": "$util.escapeJavaScript($input.json('$.userId'))" },
+              'roomId': {"S": "$util.escapeJavaScript($input.json('$.roomId'))" }
+            }       
+          })
+        },
+        integrationResponses: [
+          {
+            statusCode: '200',
+          },
+          {
+            statusCode: '400',
+          },
+          {
+            statusCode: '500',
+          }
+        ]
+      },
+    });
+
+    const writing = api.root.addResource('addingItem');
+    
+    writing.addMethod('POST', sendMessageIntegration, {
+      requestParameters: {
+        'method.request.querystring.who': true
+      },
+      requestValidatorOptions: {
+        requestValidatorName: 'test-validator',
+        validateRequestBody: true,
+        validateRequestParameters: false
+      },
+      methodResponses: [
+        {
+          statusCode: '400',
+        },
+        {
+          statusCode: '200',
+        },
+        {
+          statusCode: '500',
+        }
+      ]
+    });
+    
   }
 }
